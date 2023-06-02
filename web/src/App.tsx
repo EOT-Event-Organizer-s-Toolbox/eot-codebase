ac961687f709fa0787a15bc67c6c49c0af747525
Dale Orders <daleorders7@gmail.com>
2023-06-03 02:51:54 +1000

    installed and wrote prettier tests, updated github actions to test formatting and formatted both frontend and backend

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,11 @@
-import './App.css'
+import './App.css';
 
 function App() {
-
   return (
     <>
       <h1>Event Organizer's Toolbox</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
