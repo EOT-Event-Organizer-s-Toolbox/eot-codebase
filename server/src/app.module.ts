1493b5254f2891cff209255e6da4d1e3beb7ba34
Danny Kim <0916dhkim@gmail.com>
2023-06-04 15:52:57 +0000

    Use PORT env

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        PORT: Joi.number().default(3000),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  static port: number;
+
+  constructor(configService: ConfigService) {
+    console.log(configService.get('PORT'));
+    AppModule.port = configService.get('PORT');
+  }
+}
