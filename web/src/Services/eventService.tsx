cf89706bfc67c4e25660200fbfc25b6e694dd84e
Mike Vautour <mike@iglab.dev>
2023-06-04 08:39:42 -0400

    wip - prettier errors

--- web/src/Services/eventService.tsx
+++ web/src/Services/eventService.tsx
@@ -1,24 +1,28 @@
 import axios from 'axios';
+import { EventType } from '../types';
+
 const baseUrl = 'http://localhost:3001/events';
 
 // Retrieve all Events from the server
 const getAll = async () => {
   try {
     const req = await axios.get(baseUrl);
-    return req.data;
-  } catch (e) {
-    console.error(e);
-  }
-}
-// Retrieve a single event from the server
-const getEvent = async (id: number) => {
-  try {
-    const req = await axios.get(`${baseUrl}/${id}`);
-    return req.data;
+    const eventList: EventType[] = req.data;
+    return eventList;
   } catch (e) {
     console.error(e);
   }
 };
 
+// Retrieve a single event from the server
+const getEvent = async (id: number) => {
+  try {
+    const req = await axios.get(`${baseUrl}/${id}`);
+    const event: EventType = req.data;
+    return event;
+  } catch (e) {
+    console.error(e);
+  }
+};
 
 export default { getAll, getEvent };
