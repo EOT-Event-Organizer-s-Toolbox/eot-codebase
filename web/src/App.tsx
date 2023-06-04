5ea957683090f67e4d0184c170a35986e733150a
Mike Vautour <mike@iglab.dev>
2023-06-04 09:34:02 -0400

    prettier fix and write scripts, Formatted files

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,19 +2,19 @@ import { useState, useEffect } from 'react';
 import { EventType } from './types';
 
 import './App.css';
-import eventService from './Services/eventService'
-import EventList from './Components/EventList'
+import eventService from './Services/eventService';
+import EventList from './Components/EventList';
 
 function App() {
-  const [eventList, setEventsList] = useState<EventType[]>([])
+  const [eventList, setEventsList] = useState<EventType[]>([]);
 
-  useEffect(() => { 
+  useEffect(() => {
     const fetchEvents = async () => {
       const allEvents = await eventService.getAll();
       setEventsList(allEvents);
-    }
+    };
     fetchEvents();
-  }, [])
+  }, []);
   return (
     <main>
       <h1>Event Organizer's Toolbox</h1>
--- web/src/Components/EventList/index.tsx
+++ web/src/Components/EventList/index.tsx
@@ -2,16 +2,16 @@ import EventListSingle from './EventListSingle';
 import { EventType } from '../../types';
 
 interface Props {
-  eventList : EventType[]
+  eventList: EventType[];
 }
 
 const EventList = ({ eventList }: Props) => {
   return (
     <div className="placeholder">
       EventList
-      <EventListSingle eventList={eventList}/>
+      <EventListSingle eventList={eventList} />
     </div>
   );
-}
+};
 
 export default EventList;
