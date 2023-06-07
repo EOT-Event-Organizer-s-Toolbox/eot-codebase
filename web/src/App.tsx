4059ed5e5b61f9413087870dad61374e30488ec8
Daniela Parra <70861322+parradaniela@users.noreply.github.com>
2023-06-07 11:52:41 -0400

    Doc/readme update (#47)
    * Created automated tests using github actions. Workflow installs and runs build and unit tests. Also runs prettier formatting on both frontend and backend. (#23)
    
    * added github workflows yaml file to automatically install dependencies, build packages and run tests
    
    * updated file name
    
    * testing workflow
    
    * updated test
    
    * updated gitflow to include new versions
    
    * ran install
    
    * testing
    
    * updated version to 18.16.0
    
    * installed and wrote prettier tests, updated github actions to test formatting and formatted both frontend and backend
    
    * Updated formatting to run checks on src files
    
    * Feat 11 WIP  (#31)
    
    * testing signed commit
    
    * installed react-router-dom and types
    
    * Added Initial types
    
    * made types file too early.
    
    * Commit to test commit signing
    
    * Commit to test commit signing #2
    
    * Added EventList Comp, favicon axios, json-server
    - initial types defs
    - services for fetching events temp
    - json-server + fake data
    - outline for EventList and EventListSingle
    - svg for favicon
    - copied .prettierrc from backend
    
    * removed extra package lock
    
    * re-sync package and package-lock.json
    
    * fix formatting
    
    * wip - prettier errors
    
    * prettier fix and write scripts, Formatted files
    
    * Set state type to allow undefined
    
    * mapped events and added props
    
    * added formatting
    
    * util functions for date, structure for Component
    
    * Resolved PR Suggestions
    
    * Merge, Prettier, and content fixes for ReadMe
    
    ---------
    
    Co-authored-by: DaleOrders <52617475+DaleOrders@users.noreply.github.com>
    Co-authored-by: Mike Vautour <mike@iglab.dev>

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,27 @@
-import './App.css'
+import { useState, useEffect } from 'react';
+import { EventType } from './types';
+
+import './App.css';
+import eventService from './Services/eventService';
+import EventList from './Components/EventList';
 
 function App() {
+  const [eventList, setEventsList] = useState<EventType[] | undefined>([]);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      const allEvents = await eventService.getAll();
+      setEventsList(allEvents);
+    };
+    fetchEvents();
+  }, []);
 
   return (
-    <>
+    <main>
       <h1>Event Organizer's Toolbox</h1>
-    </>
-  )
+      <EventList eventList={eventList} />
+    </main>
+  );
 }
 
-export default App
+export default App;
