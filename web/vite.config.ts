2e76bb8beb9880ceef6fba6cc1ec06a5a7e198db
Mike Vautour <mike@iglab.dev>
2023-07-05 15:43:40 -0400

    fix hot reload of TSX files

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      include: '**/*.tsx',
+    })
+  ],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+  },
 });
