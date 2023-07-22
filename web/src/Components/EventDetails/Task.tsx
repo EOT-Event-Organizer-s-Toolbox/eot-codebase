909ecf32b446f8ee82988c519a5a67820fbd38aa
Mike V <mike@iglab.dev>
2023-07-22 08:50:22 -0400

    gave prop a more specific name

--- web/src/Components/EventDetails/Task.tsx
+++ web/src/Components/EventDetails/Task.tsx
@@ -1,15 +1,15 @@
 type TaskProps = {
   label: string;
-  data: boolean | undefined;
+  isComplete: boolean | undefined;
 }
 
-const Task = ({label, data} : TaskProps)  => {
+const Task = ({label, isComplete} : TaskProps)  => {
   return (
       <div className="flex flex-row gap-2">
         <h3 className="font-semibold">{label}?</h3>
         
         <p className="font-black">
-          {data ? <span className="text-lime-600">Yes</span> : <span className="text-red-500">No</span>}
+          {isComplete ? <span className="text-lime-600">Yes</span> : <span className="text-red-500">No</span>}
         </p>
       </div>
     );
