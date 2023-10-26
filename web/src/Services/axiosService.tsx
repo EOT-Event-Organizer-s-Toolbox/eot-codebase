7affc1744a5ba30652c061e47636fb3498c898b8
Danny Kim <0916dhkim@gmail.com>
2023-10-25 23:47:11 -0400

    Reverse proxy for dev server (#235)

--- web/src/Services/axiosService.tsx
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const axiosService = axios.create({
-  withCredentials: true,
-  baseURL: 'http://localhost:3000/api',
-});
-
-export default axiosService;
--- web/src/Services/eventTypeService.tsx
+++ web/src/Services/eventTypeService.tsx
@@ -1,12 +1,12 @@
-import axiosService from './axiosService';
+import axios from 'axios';
 import { CommunityEventType } from '../types';
 
-const baseUrl = '/event-types';
+const baseUrl = '/api/event-types';
 
 /* Retrieve all EventTypes from the server */
 const getAll = async () => {
   try {
-    const req = await axiosService.get(baseUrl);
+    const req = await axios.get(baseUrl);
     const eventTypes: CommunityEventType[] = req.data.data;
     return eventTypes;
   } catch (e) {
